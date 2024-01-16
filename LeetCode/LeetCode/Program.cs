b14af316915ad8e7f4888fb70c50c1f027efd20a
Mery <yazmery@gmail.com>
2024-01-16 12:15:20 -0800

    Valid Parentheses problem solved
    Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.

---
+++ LeetCode/LeetCode/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace LeetCode
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ValidParentheses parentheses = new ValidParentheses();
+            bool result = parentheses.IsValid("(){}}{");
+            Console.WriteLine($"Is Valid: {result}");
+
+        }
+    }
+}
---
+++ LeetCode/LeetCode/ValidParentheses.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    public class ValidParentheses
+    {        
+        public bool IsValid(string s)
+        {
+            //First validation
+            if (s.Length % 2 != 0)
+            {
+                return false;
+            }
+            // Second
+            if (s[0] == ')' || s[0] == '}' || s[0] == ']') 
+            {
+                return false;
+            }
+
+            string[] parenthesesTypes = new string[s.Length];
+            int j = 0;
+
+            for (int i = 0; i < s.Length; i++)
+            {               
+                bool openingParentheses = false;
+                bool closingParentheses = false;
+                bool openingSquareBracket = false;
+                bool closingSquareBracket = false;
+                bool openingBraces = false;
+                bool closingBraces = false;
+
+                switch (s[i])
+                {
+                    case '(':
+                        openingParentheses = true;
+                        break;
+                    case ')':
+                        closingParentheses = true;
+                        break;
+                    case '[':
+                        openingSquareBracket = true;
+                        break;
+                    case ']':
+                        closingSquareBracket = true;
+                        break;
+                    case '{':
+                        openingBraces = true;
+                        break;
+                    case '}':
+                        closingBraces = true;
+                        break;
+                    default:
+                        break;
+                }
+                
+                if (openingParentheses)
+                {
+                    parenthesesTypes[j] = "openingParentheses";
+                    j++;
+                }
+                if (closingParentheses)
+                {
+                    if (j == 0)
+                    {
+                        return false;
+                    }
+                    if (parenthesesTypes[j-1] == "openingParentheses")
+                    {
+                       j--;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+                if (openingSquareBracket)
+                {
+                    parenthesesTypes[j] = "openingSquareBracket";
+                    j++;
+                }
+                if (closingSquareBracket)
+                {
+                    if (j == 0)
+                    {
+                        return false;
+                    }
+                    if (parenthesesTypes[j - 1] == "openingSquareBracket")
+                    {
+                        j--;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+                if (openingBraces)
+                {
+                    parenthesesTypes[j] = "openingBraces";
+                    j++;
+                }
+                if (closingBraces)
+                {
+                    if (j == 0)
+                    {
+                        return false;
+                    }
+                    if (parenthesesTypes[j - 1] == "openingBraces")
+                    {
+                        j--;
+                    }
+                    else
+                    {
+                        return false;
+                    }                    
+                }
+            }
+            if(j > 0)
+            {
+                return false;
+            }
+            return true;
+        }
+
+    }
+}
